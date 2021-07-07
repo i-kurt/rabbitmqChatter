48d98c44b4a446b13e634c103a8641eb655c98d0
i-kurt <ibrahimkurtcompeng@gmail.com>
2021-07-07 11:04:29 +0300

    test2

--- rabbitmqChatter/Program.cs
+++ rabbitmqChatter/Program.cs
@@ -14,7 +14,7 @@ namespace rabbitmqChatter
         [STAThread]
         static void Main()
         {
-            //test
+            //test2
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmChatter());
